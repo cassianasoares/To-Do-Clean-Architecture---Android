2b3a6914b6868b6c5fd9b6f349f3cc25820a9a32
cassianasoares <cassianassilva@hotmail.com>
2020-10-03 14:24:24 -0300

    navigation component commit

---
+++ app/src/main/java/com/demo/android/todoappcleanarchitecture/fragments/add/AddFragment.kt
@@ -0,0 +1,25 @@
+package com.demo.android.todoappcleanarchitecture.fragments.add
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.demo.android.todoappcleanarchitecture.R
+
+class AddFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        setHasOptionsMenu(true)
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
+
+}
---
+++ app/src/main/java/com/demo/android/todoappcleanarchitecture/fragments/update/UpdateFragment.kt
@@ -0,0 +1,24 @@
+package com.demo.android.todoappcleanarchitecture.fragments.update
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.demo.android.todoappcleanarchitecture.R
+
+class UpdateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //Set New Menu
+        setHasOptionsMenu(true)
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
+}
